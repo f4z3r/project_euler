d8598ae9335a816971dc46efe7d3cb5eb495e5db
Jakob Beckmann <beckmann_jakob@hotmail.fr>
2018-07-30 12:39:43 +0200

    added cr problem 002

--- crystal/spec/crystal_spec.cr
+++ crystal/spec/crystal_spec.cr
@@ -1,7 +1,11 @@
 require "./spec_helper"
 
 describe Crystal do
-  it "problem 001 works" do
+  it "solution 001 works" do
     P001.run(10).should eq(23)
+  end
+
+  it "solution 002 works" do
+    P002.run(100).should eq(44)
   end
 end
