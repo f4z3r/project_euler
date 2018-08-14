01a14ad24c7db78b1e532f637050d096f6ffa6a6
Jakob Beckmann <beckmann_jakob@hotmail.fr>
2018-08-14 12:13:09 +0200

    added rs problem 002

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,2 +1,6 @@
 #![allow(dead_code)]
+
+mod utils;
+
 mod p001_multiples;
+mod p002_even_fib;
---
+++ rust/src/utils.rs
@@ -0,0 +1,57 @@
+//! utility module containing shared functionality for the problems
+
+pub struct FibRange {
+    curr: u64,
+    next: u64,
+    max: Option<u64>,
+    max_count: Option<u64>,
+    count: u64,
+}
+
+impl FibRange {
+    pub fn new() -> FibRange {
+        FibRange {
+            curr: 0,
+            next: 1,
+            max: None,
+            max_count: None,
+            count: 0,
+        }
+    }
+
+    /// Makes the iterator stop when the value of the returned fibonacci number has reached max.
+    pub fn with_max(max: u64) -> FibRange {
+        let mut range = FibRange::new();
+        range.max = Some(max);
+        range
+    }
+
+    /// Makes the iterator stop when the number of returned values has reached the limit.
+    pub fn with_count_limit(max: u64) -> FibRange {
+        let mut range = FibRange::new();
+        range.max_count = Some(max);
+        range
+    }
+}
+
+impl Iterator for FibRange {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<u64> {
+        let temp = self.curr;
+        self.curr = self.next;
+        self.next += temp;
+
+        if let Some(max_count) = self.max_count {
+            if max_count <= self.count { return None }
+        }
+        if let Some(max) = self.max {
+            if max <= self.curr { return None }
+        }
+
+        self.count += 1;
+
+        Some(self.curr)
+    }
+}
+
