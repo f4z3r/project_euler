ee2a761d8f05db92980fad1f9732293b06cd0ea0
Jakob Beckmann <beckmann_jakob@hotmail.fr>
2018-12-26 16:18:11 +0100

    Fixed logical error in problem 003 scala

--- scala/src/main/scala/example/p003_largest_prime.scala
+++ scala/src/main/scala/example/p003_largest_prime.scala
@@ -6,7 +6,7 @@ object P003 {
   }
 
   private def primeFactors(a: Int): List[Int] = {
-    val exists = (2 to math.sqrt(a).toInt).find(a % _ == 0)
+    val exists = (2 to a).find(a % _ == 0)
     exists match {
       case Some(d) => d :: primeFactors(a / d)
       case None    => List(a)
